99a07578061e310324af9c52f7a039f86dc05813
srilalithaa <srilalitha1004@gmail.com>
2025-05-12 21:56:13 +0530

    initial commit

---
+++ eclipse-workspace/ModelProject/src/workSpace/LaunchBrowser.java
@@ -0,0 +1,20 @@
+package workSpace;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class LaunchBrowser {
+	public static void main(String[]args) {
+	WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
+	}
+}
