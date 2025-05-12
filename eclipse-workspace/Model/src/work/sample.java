7dd46e7c1b658424775076d0da2a8f26a33aa197
srilalithaa <srilalitha1004@gmail.com>
2025-05-12 22:00:58 +0530

    Initial Commit

---
+++ eclipse-workspace/Model/src/work/sample.java
@@ -0,0 +1,16 @@
+package work;
+
+import org.apache.commons.lang3.RandomStringUtils;
+
+public class sample {
+	public static void main(String[]args) {
+		String generatedString = RandomStringUtils.randomAlphanumeric(100);
+
+	    System.out.println(generatedString);
+		
+				
+		
+	}
+
+
+}
